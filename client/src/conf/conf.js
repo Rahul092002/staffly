90a4c7a114377a98b32bdfe5aae05e231394cbb7
Rahul Sinha <sinha7rahul@gmail.com>
2025-10-15 23:45:56 +0530

    Update conf.js

--- client/src/conf/conf.js
+++ client/src/conf/conf.js
@@ -5,6 +5,7 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     appwriteJWT: String(import.meta.env.VITE_APPWRITE_JWT),
+    backendUrl: String(import.meta.env.VITE_SERVER_URL),
 }
 
 
